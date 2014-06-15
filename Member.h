42408e0d8ad90fe8810ca5652a755eca2c6d87ba
030ii <lovelygeee2@gmail.com>
2014-06-15 19:20:13 +0900

    final2

--- Member.h
+++ Member.h
@@ -12,8 +12,9 @@ void printBox(char txt[]);
 void menu1_member_view(Tree *tree);
 void menu2_member_add(Tree *tree, Tree *treeForName);
 void menu3_member_modify(Tree *tree, Tree *treeForName);
-void menu4_member_delete(Tree *tree, Tree *treeForName); 
+void menu4_member_delete(Tree *tree, Tree *treeForName);
 void menu6_data_save(Tree *tree);
+void menu0_close();
 
 int isValidName(char txt[]);
 int isValidAddr(char txt[]);
